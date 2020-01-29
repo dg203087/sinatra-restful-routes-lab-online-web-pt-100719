907cb337b836d91eb5b4f9e81cb243bfa3d02df6
Deanna Kelley <deanna.georges@gmail.com>
2020-01-29 03:47:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
-  #
+  #patch action
   patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
@@ -49,6 +49,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
   
+  #delete action 
   delete '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
